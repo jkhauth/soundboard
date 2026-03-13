268aacc05133bcb952665c470da554ec830c5020
John H <jkhauth@gmail.com>
2026-03-12 22:10:13 -0400

    url changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // Replace 'bachelor-board' with your actual repository name on GitHub
-  base: '/soundboard/', 
+  base: '/', 
   plugins: [react()],
 })
